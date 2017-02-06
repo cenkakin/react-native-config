fa5cf960ef40b17890f8c97a9c575e41382e87e7
cenk akın <cenk.akin91@gmail.com>
2017-02-06 14:59:36 +0300

    Index default values (#2)
    * update ios headers for react native 0.40
    
    * v0.2.2
    
    * revert rn 0.4 compat, leaving this for 0.3
    
    * v0.2.5
    
    * bringing rn 0.40 compat
    
    * v0.3.1
    
    * env config files are read and exported before preparing flavor's dependencies for android
    
    * type conversion and array supports are added

--- index.js
+++ index.js
@@ -5,4 +5,22 @@
 // iOS: config vars set in xcconfig and exposed via ReactNativeConfig.m
 import { NativeModules } from 'react-native';
 
-export default NativeModules.ReactNativeConfig || {};
+const Config = NativeModules.ReactNativeConfig || {};
+
+const parsePropsToObjects = () => {
+  Object.keys(Config).forEach(conf => {
+    try {
+      Config[conf] = JSON.parse(Config[conf]);
+} catch (e) {
+  }
+});
+  return Config;
+};
+
+const parsedConfig = parsePropsToObjects();
+
+export function getDefaultConfig(propName) {
+  return parsedConfig[propName] instanceof Array ? parsedConfig[propName][0] : parsedConfig[propName];
+}
+
+export default parsedConfig;
